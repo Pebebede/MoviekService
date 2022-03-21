b18124baf052f4fdc76a140f7ff5ffa2365be190
pebe <necromos@gmail.com>
2022-03-21 21:56:17 +0100

    PB-2 add springboot

---
+++ src/main/java/com/example/Moviek/MoviekApplication.java
@@ -0,0 +1,13 @@
+package com.example.Moviek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Moviek/MoviekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Moviek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
